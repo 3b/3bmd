dcaf1d6f8b97567c93239be9d1787ae8518e897d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-12-26 23:03:47 +0300

    Add a special macro for defining grammar tests.

--- tests/grammar/bullet-list.lisp
+++ tests/grammar/bullet-list.lisp
@@ -1,87 +1,69 @@
 (in-package 3bmd-tests)
 
 
-(deftest parse-simple-list ()
-  (let ((expected '((:BULLET-LIST
-                     (:LIST-ITEM (:PLAIN "Foo"))
-                     (:LIST-ITEM (:PLAIN "Bar"))))))
-    (multiple-value-bind (result remaining-text parse-succeeded)
-        (3bmd-grammar:parse-doc ' "* Foo
+(def-grammar-test parse-simple-list
+  :text "* Foo
 * Bar
-")
-      (is (equalp result expected))
-      (is (not remaining-text))
-      (is parse-succeeded))))
+"
+  :expected '((:BULLET-LIST
+               (:LIST-ITEM (:PLAIN "Foo"))
+               (:LIST-ITEM (:PLAIN "Bar")))))
 
-
-(deftest parse-list-with-separated-items ()
-  (let ((expected '((:BULLET-LIST
-                     (:LIST-ITEM (:PARAGRAPH "Foo"))
-                     (:LIST-ITEM (:PARAGRAPH "Bar"))))))
-    (multiple-value-bind (result remaining-text parse-succeeded)
-        (3bmd-grammar:parse-doc "* Foo
+(def-grammar-test parse-list-with-separated-items
+  :text "* Foo
 
 * Bar
-")
-      (is (equalp result expected))
-      (is (not remaining-text))
-      (is parse-succeeded))))
+"
+  :expected '((:BULLET-LIST
+               (:LIST-ITEM (:PARAGRAPH "Foo"))
+               (:LIST-ITEM (:PARAGRAPH "Bar")))))
 
 
-(deftest parse-list-with-multiline-item ()
-  (let ((expected '((:BULLET-LIST
-                     (:LIST-ITEM
-                      ;; TODO:
-                      ;; Seems in this case this item should be a PARAGRAPH too?
-                      (:PLAIN "Foo")))
-                    ;; TODO:
-                    ;; Why this paragraph is not part of the previos list item?
-                    ;; It has the same indentation as the list item.
-                    (:PARAGRAPH "Second" " " "line")
-                    ;; TODO:
-                    ;; Why do this "Bar is not a second LIST-ITEM of the first BULLET-LIST?
-                    (:BULLET-LIST
-                     (:LIST-ITEM (:PLAIN "Bar"))))))
-    (multiple-value-bind (result remaining-text parse-succeeded)
-        (3bmd-grammar:parse-doc "* Foo
+(def-grammar-test parse-list-with-multiline-item
+  :text "* Foo
 
   Second line
 
 * Bar
-")
-      (is (equalp result expected))
-      (is (not remaining-text))
-      (is parse-succeeded))))
+"
+  :expected '((:BULLET-LIST
+               (:LIST-ITEM
+                ;; TODO:
+                ;; Seems in this case this item should be a PARAGRAPH too?
+                (:PLAIN "Foo")))
+              ;; TODO:
+              ;; Why this paragraph is not part of the previos list item?
+              ;; It has the same indentation as the list item.
+              (:PARAGRAPH "Second" " " "line")
+              ;; TODO:
+              ;; Why do this "Bar is not a second LIST-ITEM of the first BULLET-LIST?
+              (:BULLET-LIST
+               (:LIST-ITEM (:PLAIN "Bar")))))
 
 
-(deftest parse-list-with-embedded-code ()
-  (let ((expected '((:BULLET-LIST (:LIST-ITEM (:PLAIN "Foo")))
-                    ;; TODO:
-                    ;; This paragraph should be a part of the first list item
-                    (:PARAGRAPH
-                     (:CODE "
-  This is a code
-"))
-                    ;; And this bullet list should not be generated.
-                    ;; The Bar list item should be inside the first bullet list.
-                    (:BULLET-LIST (:LIST-ITEM (:PLAIN "Bar")))))
-    ;;     (real-expected '((:BULLET-LIST
-    ;;                       (:LIST-ITEM
-    ;;                        (:PARAGRAPH "Foo")
-    ;;                        (:PARAGRAPH (:CODE "
-    ;;   This is a code
-    ;; ")))
-    ;;                  (:LIST-ITEM (:PLAIN "Bar")))))
-        )
-    (multiple-value-bind (result remaining-text parse-succeeded)
-        (3bmd-grammar:parse-doc "* Foo
+(def-grammar-test parse-list-with-embedded-code
+  :text "* Foo
 
   ```
   This is a code
   ```
 
 * Bar
-")
-      (is (equalp result expected))
-      (is (not remaining-text))
-      (is parse-succeeded))))
+"
+  ;; :really-expected '((:BULLET-LIST
+  ;;                     (:LIST-ITEM
+  ;;                      (:PARAGRAPH "Foo")
+  ;;                      (:PARAGRAPH (:CODE "
+  ;;         This is a code
+  ;;       ")))
+  ;;                     (:LIST-ITEM (:PLAIN "Bar"))))
+  :expected '((:BULLET-LIST (:LIST-ITEM (:PLAIN "Foo")))
+              ;; TODO:
+              ;; This paragraph should be a part of the first list item
+              (:PARAGRAPH
+               (:CODE "
+  This is a code
+"))
+              ;; And this bullet list should not be generated.
+              ;; The Bar list item should be inside the first bullet list.
+              (:BULLET-LIST (:LIST-ITEM (:PLAIN "Bar")))))
